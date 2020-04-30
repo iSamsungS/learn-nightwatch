1b1a71a41617310d0acb3c1b07d37c87729df0ba
Siim Tiilen <eritikass@gmail.com>
2020-04-30 14:51:36 +0300

    add wait example

--- test/e2e/auto24-kasutatud.js
+++ test/e2e/auto24-kasutatud.js
@@ -7,21 +7,30 @@ module.exports = {
       .url("https://www.auto24.ee/kasutatud/kasutatud.php")
       .waitForElementVisible("body");
 
+
+    // browser.click('#searchParam-show_pictures').pause(1500);
+
     // browser.element("css selector", "#searchParam-show_pictures", (result) => {
     //   console.log("result", result);
     // });
 
-    browser.moveToElement('#searchParam-show_pictures', 0, 0).pause(3500);
+    browser.moveToElement("#searchParam-show_pictures", 0, 0).pause(3500);
 
     browser.elements(
       "css selector",
       "#searchParam-show_pictures:not(:checked)",
       (result) => {
+        console.log("count", result.value.length);
         if (result.value.length === 1) {
+          console.log("-click");
           browser.click("#searchParam-show_pictures:not(:checked)");
         }
       },
     );
+
+    console.log("a");
+    browser.waitForElementVisible("#searchParam-show_pictures:checked");
+    console.log("b");
 
     browser.pause(3000).end();
   },
